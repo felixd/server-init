65ce8e5c6b89e18e53234f2bff6e414f0ccbbb21
Paweł Wojciechowski <felixd@konopnickiej.com>
2025-05-06 15:18:32 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@
 ssh_keys=(
     "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAEHbtCr6gO2fvELCP3tN7opQ4YUpEKsU/MxqJMJIz+VVlJQ6uiq1Lm2HDgU45SM1AhKASMmWnKm4tPCcTwzvPd4MwAuReslHE/ahtNNBnbjGu90nLe7SVn1aytaeKC2rYo8XH5/xa1hOpzBG7Zj6Qp/ihjuWkVIPwS/rZBUvdjtlk9oFQ== felixd-GPG"
     "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBHEmnlM6yvgEx49xFjZPchL5/elqHL5ojqehcObLFqSKBHYrYklyPvA5z0jYV54GmFJ5ZxWHpe+jQr/sw9i2tnqvwaAKGqvX7aK06h9p9NbQ1j2JIB8r9xjou366a6ZjUA==  PKCS:ceac18fae62357cafc0f0f842b44f702bf29265b= felixd-eID-SC"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZ+JLxvk0QVurxFXUgssEvzWTQTxYUzMKxxR0l+AMI3i4T633Hx9PB5Q8Lf526iaoSNTfcU3EJ8J/CHbi8jDfmFeVjB17XAo3G8cdTPrvDJRjzpUM66o8vgryumwUjYC5rYlF+Qn5WXnKut5hSXtHHMvfVy2EavtCvvvLUkgIddzH+3faclugI4zlOskXdt+TeUYG+fuLXAKePW4TqUx67iNGcBKg8c83TWmfX4R1e7M7+gz03cqfdK6LzfXVK+keRygeHdohfAcwbhFZjmvY/AR+3lFAGAwUwIemXiuM/8728qa7rHTVgGkbMhowx7a55TcaUURYjl58QgyGw2/XUmjnAjk4veKf0H8TjMS05Sag/vcpi0rM0qDgUfXElEtmzEYUGpgYCVTm6MjgYqnnjIfrJssLRRUMnDLCivxEi5a8K3Vt73iv9Z/LVzQnftEFSeohC4fZpxGhjiQje/G9gVButMzARwii0JUYuYLXZEssfc/Q3oMqwkGRkcSqNaYE= felixd@sur10-ser-dl360-gen9-1"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObGx+p6XO+X49dxjCjNNpfV/HC99FP7+MmsrYpTOyLh felixd@felixd-ilogic-terminal"
     # Add more keys as needed
 )
 
@@ -46,4 +48,3 @@ if [ -f /etc/os-release ]; then
 else
     echo "Unable to determine the distribution type."
 fi
-
