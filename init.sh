aaaf24ac054e96e9544141b081962605aeffb278
Paweł Wojciechowski <felixd@konopnickiej.com>
2025-05-06 23:16:50 +0200

    klucz laptop

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ ssh_keys=(
     "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBHEmnlM6yvgEx49xFjZPchL5/elqHL5ojqehcObLFqSKBHYrYklyPvA5z0jYV54GmFJ5ZxWHpe+jQr/sw9i2tnqvwaAKGqvX7aK06h9p9NbQ1j2JIB8r9xjou366a6ZjUA==  PKCS:ceac18fae62357cafc0f0f842b44f702bf29265b= felixd-eID-SC"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZ+JLxvk0QVurxFXUgssEvzWTQTxYUzMKxxR0l+AMI3i4T633Hx9PB5Q8Lf526iaoSNTfcU3EJ8J/CHbi8jDfmFeVjB17XAo3G8cdTPrvDJRjzpUM66o8vgryumwUjYC5rYlF+Qn5WXnKut5hSXtHHMvfVy2EavtCvvvLUkgIddzH+3faclugI4zlOskXdt+TeUYG+fuLXAKePW4TqUx67iNGcBKg8c83TWmfX4R1e7M7+gz03cqfdK6LzfXVK+keRygeHdohfAcwbhFZjmvY/AR+3lFAGAwUwIemXiuM/8728qa7rHTVgGkbMhowx7a55TcaUURYjl58QgyGw2/XUmjnAjk4veKf0H8TjMS05Sag/vcpi0rM0qDgUfXElEtmzEYUGpgYCVTm6MjgYqnnjIfrJssLRRUMnDLCivxEi5a8K3Vt73iv9Z/LVzQnftEFSeohC4fZpxGhjiQje/G9gVButMzARwii0JUYuYLXZEssfc/Q3oMqwkGRkcSqNaYE= felixd@sur10-ser-dl360-gen9-1"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObGx+p6XO+X49dxjCjNNpfV/HC99FP7+MmsrYpTOyLh felixd@felixd-ilogic-terminal"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESTGS0PMn9hEPBJNFYrGxO06neT24BU8/peIT7ulkVs felixd@felixd-dell-late5450-terminal"
     # Add more keys as needed
 )
 
